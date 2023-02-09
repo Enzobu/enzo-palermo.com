2ba44ca08faf6cf9e1957757fd882fb4c3efaac3
Enzobu <enzo.palermo@epsi.fr>
2023-02-09 21:58:03 +0100

    Update isLoged.php

--- php/isLoged.php
+++ php/isLoged.php
@@ -2,9 +2,38 @@
 
 session_start();
 
-if (!array_key_exists('id', $_SESSION)) {
-    header('location: ../login/?error=notLog');
-    die();
+$sql = "SELECT * FROM `users_enzo_palermo`";
+$requete = $db->query($sql);
+define("USERS", $requete->fetchAll());
+// $users = $requete->fetchAll();
+
+function isLoged() {
+    if (array_key_exists('id', $_SESSION)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function havePermission() {
+    foreach (USERS as $user) {
+        if ($_SESSION['id'] == $user['id']) {
+            return $user['permission'];
+        }
+    }
+}
+
+function itsAllowed($permissionRequired) {
+    if (isLoged()) {
+        if (!(havePermission() >= $permissionRequired)) {
+            // header('location: ../login/?error=notAllowed');
+            header("Refresh: 0;url=/login/?error=notAllowed");
+            die();
+        }
+    } else {
+        header('Refresh: 0;url=/login/?error=notLog');
+        die();
+    }
 }
 
 ?>
