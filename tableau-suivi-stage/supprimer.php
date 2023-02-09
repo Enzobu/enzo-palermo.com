5eec66bdc280cc4810cbb51b02084a25e0555de7
Enzobu <enzo.palermo@epsi.fr>
2023-02-09 18:16:05 +0100

    Create supprimer.php

---
+++ tableau-suivi-stage/supprimer.php
@@ -0,0 +1,12 @@
+<?php
+    include("../php/isLoged.php");
+    include("../php/link_db.php");
+
+    if (array_key_exists('id', $_GET)) {
+        $id = $_GET['id'];
+        $sql = "DELETE FROM `tableau-recherche-stage` WHERE id = $id";
+        $requete = $db->query($sql);
+    }
+    
+    header("Refresh:0; url=/tableau-suivi-stage/index.php");
+    exit();
