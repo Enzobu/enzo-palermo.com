1d2ea6e76816fc2f02757ef0257fea6d9aa56aa6
Enzobu <enzo.palermo@epsi.fr>
2023-02-09 18:13:19 +0100

    js button delete

--- js/tableau_suivi_stage.js
+++ js/tableau_suivi_stage.js
@@ -3,4 +3,12 @@ function ajouter (){
     var element2 = document.getElementById("double-ligne");
     element1.classList.toggle("ligne-ajouter");
     element2.classList.toggle("ligne-ajouter");
+function effacer (id){
+    var element1 = document.getElementById(id);
+    element1.classList.toggle("ligne-modifier");
+    if (confirm("Etes vous sur de vouloir supprimer la ligne "+id)) {
+        document.location.href="supprimer.php?id="+id; 
+      } else {
+        document.location.href="/tableau-suivi-stage/index.php"; 
+      }
 }
