c869c8015a04928264391dac7ae5ae5a9f387702
Enzobu <enzo.palermo@epsi.fr>
2023-02-13 08:52:45 +0100

    Create footer.php

---
+++ php/footer.php
@@ -0,0 +1,11 @@
+<ul class="footer-list">
+    <li class="footer-nav-item"><a href="https://www.epsi.fr/"><img src="/image/logoEpsiFondBlanc.svg" alt="EPSI" class="logo-footer logo-epsi"></a></li>
+    <li class="footer-nav-item"><a href="https://github.com/Enzobu"><img src="/image/github.svg" alt="GitHub" class="logo-footer logo-github"></a></li>
+    <li class="footer-nav-item"><a href="https://www.linkedin.com/in/enzo-palermo-544720253/"><img src="/image/linkedin_bleu.svg" alt="Linkedin" class="logo-footer logo-linkedin"></a></li>
+</ul>
+<div class="footer-info">
+    <a href="/source-icones">
+        <div class="info-logo">i</div>
+        <div class="info">Sources icônes</div>
+    </a>
+</div>
