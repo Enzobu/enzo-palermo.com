f329b2277a30e7265e18bf80f96b5231df25ab30
Enzobu <enzo.palermo@epsi.fr>
2023-02-14 20:55:01 +0100

    target="blank" footer links

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,7 @@
 <ul class="footer-list">
-    <li class="footer-nav-item"><a href="https://www.epsi.fr/"><img src="/image/logoEpsiFondBlanc.svg" alt="EPSI" class="logo-footer logo-epsi"></a></li>
-    <li class="footer-nav-item"><a href="https://github.com/Enzobu"><img src="/image/github.svg" alt="GitHub" class="logo-footer logo-github"></a></li>
-    <li class="footer-nav-item"><a href="https://www.linkedin.com/in/enzo-palermo-544720253/"><img src="/image/linkedin_bleu.svg" alt="Linkedin" class="logo-footer logo-linkedin"></a></li>
+    <li class="footer-nav-item"><a href="https://www.epsi.fr/campus/campus-de-montpellier/" target="blank"><img src="/image/logoEpsiFondBlanc.svg" alt="EPSI" class="logo-footer logo-epsi"></a></li>
+    <li class="footer-nav-item"><a href="https://github.com/Enzobu" target="blank"><img src="/image/github.svg" alt="GitHub" class="logo-footer logo-github"></a></li>
+    <li class="footer-nav-item"><a href="https://www.linkedin.com/in/enzo-palermo-544720253/" target="blank"><img src="/image/linkedin_bleu.svg" alt="Linkedin" class="logo-footer logo-linkedin"></a></li>
 </ul>
 <div class="footer-info">
     <a href="/source-icones">
