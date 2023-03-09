c424a17261f2f7cd83f787088cbe50348066219e
Enzobu <enzo.palermo@epsi.fr>
2023-03-09 14:15:57 +0100

    add source icon

--- source-icones/index.php
+++ source-icones/index.php
@@ -12,13 +12,36 @@
         <?php include('../php/navbar.php'); ?>
     </header>
     <main class="main">
+        <div class="bouton-cacher-responsive" onclick="changerLeft('0');"><img src="/image/menu.svg" alt=""></div>
         <div class="title">Icônes et leurs créteurs :</div>
         <div class="mention">
-            <a href="https://www.flaticon.com/free-icons/open-menu" title="open menu icons"><img src="/image/menu.svg" alt="Open menu">Open menu icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/open-menu" title="open menu icons"><img src="/image/menu.svg" alt="Open menu icons">Open menu icons created by Freepik - Flaticon</a>
             <a href="https://www.flaticon.com/free-icons/cross-mark" title="cross mark icons"><img src="/image/croix.svg" alt="Cross mark icons">Cross mark icons created by Andrean Prabowo - Flaticon</a>
             <a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons"><img src="/image/linkedin_bleu.svg" alt="Linkedin icons">Linkedin icons created by Google - Flaticon</a>
             <a href="https://www.flaticon.com/free-icons/logout" title="logout icons"><img src="/image/exit.svg" alt="Logout icons">Logout icons created by Freepik - Flaticon</a>
             <a href="https://www.flaticon.com/free-icons/github" title="github icons"><img src="/image/github.svg" alt="Github icons">Github icons created by Freepik - Flaticon</a>
+            
+            
+            <a href="https://www.flaticon.com/free-icons/html" title="html icons"><img src="/image/html.svg" alt="html 5 icons">Html icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/css" title="css icons"><img src="/image/css-3.svg" alt="css 3 icons">Css icons created by Pixel perfect - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/sass" title="sass icons"><img src="/image/sass.svg" alt="sass icons">Sass icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/js" title="js icons"><img src="/image/js.svg" alt="JavaScript icons">Js icons created by Smashicons - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/php" title="php icons"><img src="/image/php.svg" alt="php icons">Php icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/python" title="python icons"><img src="/image/python1.svg" alt="Python icons">Python icons created by Freepik - Flaticon</a>
+            <!-- COULEUR CI-DESSOUS -->
+            <a href="https://www.flaticon.com/free-icons/python" title="python icons"><img src="/image/python.png" alt="Python icons"> Python icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/mysql" title="mysql icons"><img src="/image/mysql.svg" alt="MySQL icons">Mysql icons created by Pixel perfect - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/penguin" title="penguin icons"><img src="/image/linux1.svg" alt="Linux icons">Penguin icons created by Freepik - Flaticon</a>
+            <!-- COULEUR CI-DESSOUS -->
+            <a href="https://www.flaticon.com/free-icons/linux" title="linux icons"><img src="/image/linux.png" alt="Linux icons">Linux icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/telephone" title="telephone icons"><img src="/image/telephoner.svg" alt="Phone icons">Telephone icons created by deha21 - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/email" title="email icons"><img src="/image/email.svg" alt="Mail icons">Email icons created by Those Icons - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/globe" title="globe icons"><img src="/image/globe.svg" alt="Web icons">Globe icons created by Uniconlabs - Flaticon</a>
+            <a href="https://www.flaticon.com/fr/icones-gratuites/utilisateur" title="utilisateur icônes"><img src="/image/profil-de-lutilisateur.svg" alt="Account icons">Utilisateur icônes créées par Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/birthday" title="birthday icons"><img src="/image/date-de-naissance.svg" alt="Calendar icons">Birthday icons created by Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/fr/icones-gratuites/voiture" title="voiture icônes"><img src="/image/permis-de-conduire.svg" alt="Permis icons">Voiture icônes créées par Freepik - Flaticon</a>
+            <a href="https://www.flaticon.com/free-icons/address" title="address icons"><img src="/image/accueil.svg" alt="Location icons">Address icons created by bqlqn - Flaticon</a>
+        
         </div>
     </main>
 
