da75daaeb0f04e3a0aede338546ba44ef47c11b6
Enzobu <enzo.palermo@epsi.fr>
2023-03-09 13:01:11 +0100

    Update epsi link

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,5 @@
 <ul class="footer-list">
-    <li class="footer-nav-item"><a href="https://www.epsi.fr/campus/campus-de-montpellier/" target="blank"><img src="/image/logoEpsiFondBlanc.svg" alt="EPSI" class="logo-footer logo-epsi"></a></li>
+    <li class="footer-nav-item"><a href="https://www.epsi.fr/campus/campus-de-montpellier/" target="blank">EPSI Montpellier</a></li>
     <li class="footer-nav-item"><a href="https://github.com/Enzobu" target="blank"><img src="/image/github.svg" alt="GitHub" class="logo-footer logo-github"></a></li>
     <li class="footer-nav-item"><a href="https://www.linkedin.com/in/enzo-palermo-544720253/" target="blank"><img src="/image/linkedin_bleu.svg" alt="Linkedin" class="logo-footer logo-linkedin"></a></li>
 </ul>
